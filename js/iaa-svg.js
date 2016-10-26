cd357504369fc3bae4ccd78ee578eca1bd2b3a44
belarmin <igpol@bk.ru>
2016-10-26 23:55:32 +0400

    +

--- js/iaa-svg.js
+++ js/iaa-svg.js
@@ -1,101 +1,6 @@
 "use strict"
 
-var myWindow = new iaaWindow();
-
 var myCanvas = new iaaCanvas();
-
-// Окно //
-
-function iaaWindow(){
-	
-	var salf = this;
-	
-	//.....
-	
-	function onDocumentLoad(){
-
-		createToolbar()
-
-		// СоздатьКарту(600, 300, 325);
-		
-		//..
-		
-		// $('#svgout').mouseover(function(){setHookMouseWheel(this, mapWheelMouse, 1)})
-		// $('#svgout').mouseout(function(){setHookMouseWheel(this, mapWheelMouse)})
-		
-		// $("#svgout").mousemove(function(e){snapMouseMove(e)})
-		// $("#svgout").mouseup(function(e){snapMouseUp(e)})
-		// $("#svgout").mousedown(function(e){snapMouseDoun(e)})
-		// $("#svgout").mouseleave(function(e){snapMouseLeave(e)})
-	
-	}
-
-	function createToolbar(){
-		
-		var tolBar1 = new iaaVerticalToolBar('leftToolBar', 'aside.left'); 
-
-		var group11 = tolBar1.addGroupButton('group11');
-
-		var button111 = group11.addButton('ButtonPropertyElement', 'button'); 
-
-		var group12 = tolBar1.addGroupButton('MapCommandButton');
-
-		var button122 = group12.addButton('ButtonSelectElement', 'radio'); 
-		var button123 = group12.addButton('ButtonHand',   'radio'); 
-		var button124 = group12.addButton('ButtonTurn',   'radio'); 	
-		
-		var tolBar2 = new iaaVerticalToolBar('rightToolBar', 'aside.right'); 
-
-		var group21 = tolBar2.addGroupButton('group21');
-
-		var button211 = group21.addButton('ButtonActionPoint', 'checkbox');
-
-		var group22 = tolBar2.addGroupButton('ElementCommandButton');
-
-		var button222 = group22.addButton('ButtonAddElement', 'radio');
-		var button223 = group22.addButton('ButtonChangePosition', 'radio');
-		var button224 = group22.addButton('ButtonChangeSize', 'radio');
-
-		var group23 = tolBar2.addGroupButton('ActionPointTrendButton');
-
-		var button235 = group23.addButton('button235', 'radio');
-		var button236 = group23.addButton('button236', 'radio');
-
-		// 
-		
-		button122.check(true);
-
-		button211.check(true);
-
-		button222.check(true);
-
-		button235.check(true);
-		
-		//
-		
-		button111.click = function(e){leftToolBarPropertyElement_onclick(e)}
-		
-		button122.click = function(e){leftToolBarSelectElement_onclick(e)}
-		
-		button123.click = function(e){leftToolBarHand_onclick(e)}
-		button123.svg_mousemove = function(sx, sy){leftToolBarHand_svg_mousemove(sx, sy)}
-
-		button124.click = function(e){leftToolBarTurn_onclick(e)}
-		button124.svg_mousemove = function(sx, sy){leftToolBarTurn_svg_mousemove(sx, sy)}
-		
-		button211.click = function(e){rightToolBarActionPoint_onclick(e)}
-		
-		//
-		
-		// svg.MapCommandButton = group12;
-		// svg.ElementCommandButton = group22;
-		// svg.ActionPointTrendButton = group23;
-		
-	}
-	
-	$(document).ready(function (){onDocumentLoad()}); // При загрузке
-	
-}
 
 // Холст //
 
@@ -110,6 +15,15 @@ function iaaCanvas(){
 	var snap = undefined;
 	
 	//.....	
+	
+	this._gx = undefined;  // Сюда сохраняется дельта x при перемещении курсора
+	this._gy = undefined;  // Сюда сохраняется дельта y при перемещении курсора 
+
+	this._pressRightMouseButton = false; // Нажата правая кнопка мыши
+	this._pressLeftMouseButton = false;  // Нажата левая кнопка мыши 
+	this._pressMouseWheel = false;		 // Нажато колёсико мыши
+	
+	//.....
 	
 	this.type = 'Холст';	// тип
 	
@@ -139,16 +53,42 @@ function iaaCanvas(){
 	
 	$(window).resize(function(){onWindowsResize()});
 
-	this.snap = function(){
+	this.snap = function(){ // swg объект
 		
 		return snap;
 		
+	}
+	
+	this.repaint = function(){	// перерисовать элементы
+		
+		if (this.currentDraft != undefined){
+		
+			this.currentDraft.repaint();
+		
+		}
+	
 	}
 	
 	function onDocumentLoad(){
 		
 		$('article').append(' <svg id="'+idForm+'"></svg> ');  
 		
+		//..
+		
+		$(selector).mouseover(function(){setHookMouseWheel(this, mapWheelMouse, 1)})
+		$(selector).mouseout(function(){setHookMouseWheel(this, mapWheelMouse)})
+		
+		$(selector).mousemove(function(e){snapMouseMove(e)})
+		$(selector).mouseup(function(e){snapMouseUp(e)})
+		$(selector).mousedown(function(e){snapMouseDoun(e)})
+		$(selector).mouseleave(function(e){snapMouseLeave(e)})		
+		
+		//..
+		
+		createToolbar()
+		
+		//..
+
 		snap = Snap(selector);
 		
 		snap.attr({fill: "#fff", "stroke-width": "1px"});
@@ -156,6 +96,20 @@ function iaaCanvas(){
 		onWindowsResize();
 		
 		salf.currentDraft = new iaaDraft('Новый проект', 600, 300, 325);
+		
+		salf.repaint();
+		
+		//.Времяночка..
+		
+		salf.currentDraft.addElement(
+			new elementProperty('Новый Элемент 1', 100, 100, 100, 200, 100, 50,0,0,0)
+		);
+
+		salf.currentDraft.addElement(
+			new elementProperty('Новый Элемент 2', 300, 300, 300, 100, 50, 150,0,0,0)
+		);
+		
+		//.Времяночка..
 		
 	}
 	
@@ -173,6 +127,274 @@ function iaaCanvas(){
 
 	}
 
+	
+	
+	//.Tool.bar.........
+	
+	function createToolbar(){
+		
+		var tolBar1 = new iaaVerticalToolBar('leftToolBar', 'aside.left'); 
+
+		var group11 = tolBar1.addGroupButton('group11');
+
+		var button111 = group11.addButton('ButtonPropertyElement', 'button'); 
+
+		var group12 = tolBar1.addGroupButton('MapCommandButton');
+
+		var button122 = group12.addButton('ButtonSelectElement', 'radio'); 
+		var button123 = group12.addButton('ButtonHand',			 'radio'); 
+		var button124 = group12.addButton('ButtonTurn',			 'radio'); 	
+		
+		var tolBar2 = new iaaVerticalToolBar('rightToolBar',	 'aside.right'); 
+
+		var group21 = tolBar2.addGroupButton('group21');
+
+		var button211 = group21.addButton('ButtonActionPoint', 'checkbox');
+
+		var group22 = tolBar2.addGroupButton('ElementCommandButton');
+
+		var button222 = group22.addButton('ButtonAddElement',		'radio');
+		var button223 = group22.addButton('ButtonChangePosition',	'radio');
+		var button224 = group22.addButton('ButtonChangeSize',		'radio');
+
+		var group23 = tolBar2.addGroupButton('ActionPointTrendButton');
+
+		var button235 = group23.addButton('button235', 'radio');
+		var button236 = group23.addButton('button236', 'radio');
+
+		// 
+		
+		button122.check(true);
+
+		button211.check(true);
+
+		button222.check(true);
+
+		button235.check(true);
+
+		//
+
+		button111.click = function(e){leftToolBarPropertyElement_onclick(e)}
+
+		button122.click = function(e){leftToolBarSelectElement_onclick(e)}
+
+
+		button123.click = function(e){leftToolBarHand_onclick(e)}
+
+		button123.svg_mousemove = function(sx, sy){leftToolBarHand_svg_mousemove(sx, sy)}
+
+
+		button124.click = function(e){leftToolBarTurn_onclick(e)}
+
+		button124.svg_mousemove = function(sx, sy){leftToolBarTurn_svg_mousemove(sx, sy)}
+
+
+		button211.click = function(e){rightToolBarActionPoint_onclick(e)}
+
+		//
+
+		salf.MapCommandButton = group12;
+		salf.ElementCommandButton = group22;
+		salf.ActionPointTrendButton = group23;
+
+	}
+	
+	function leftToolBarPropertyElement_onclick(e){
+		
+		if (salf.currentElement.type == 'Холст') {
+
+			var forma = new formPropertyMap();
+		
+			forma.open(salf.currentDraft);
+
+		}else{
+
+			if (salf.formPropertyElement == undefined) {
+
+				salf.formPropertyElement = new formPropertyElement();
+
+			}
+
+			salf.formPropertyElement.open(salf.currentElement);
+
+		}
+
+		cancelEvent(e);
+
+	}
+
+	function leftToolBarSelectElement_onclick(e){
+		
+		$("#svgout").css("cursor", "default")
+		
+		//svg.activeButton = but;
+		
+	}
+
+	function leftToolBarHand_onclick(e){
+		
+		$("#svgout").css("cursor", "url('images/hand.cur'), auto")
+		
+		//svg.activeButton = but;
+
+	}
+
+	function leftToolBarHand_svg_mousemove(sx, sy){
+		
+		if (salf._pressLeftMouseButton == true || salf._pressRightMouseButton == true){
+
+			var m = salf.scale;
+
+			var value = salf.currentDraft.Позиция
+
+			var [x, y, z] = value;
+
+			var property = {'Позиция':[sx * m, sy * m, 0]}
+
+			ОбновитьОтображение(undefined, property)
+
+		}
+
+	}
+
+	function leftToolBarTurn_onclick(e){
+		
+		$("#svgout").css("cursor", "url('images/update.png'), auto")
+		
+		//svg.activeButton = but;
+		
+	}
+
+	function leftToolBarTurn_svg_mousemove(sx, sy){
+
+		if (salf._pressLeftMouseButton == true || salf._pressMouseWheel == true){
+
+			var value = svg.Поворот
+
+			var [rx, ry, rz] = value;
+
+			value[0] = rx + (sy/4);
+			value[1] = ry - (sx/4);
+			
+			ОбновитьОтображение()
+			
+		}
+
+	}
+
+	function rightToolBarActionPoint_onclick(e){
+
+		ОбновитьОтображение()
+		
+	}
+	
+	
+	function mapWheelMouse(e) {
+
+		var i = new mauseWheelEvent(e)
+
+		var [ScaleMin, ScaleMax, ScaleStep] = [0.05, 1.5, 0.1];	
+		
+		var newScale = salf.scale;
+		
+		if (i.value > 0){
+
+			newScale = salf.scale - ScaleStep;
+			
+		}else{
+
+			newScale = salf.scale + ScaleStep;
+
+		}
+
+		if (newScale >= ScaleMin && newScale <= ScaleMax){
+		
+			salf.scale = newScale;
+		
+			ОбновитьОтображение()
+			
+		}
+		
+		return cancelEvent(e);
+
+	}
+
+	function inputWheelMouse(e){
+
+		var i = new mauseWheelEvent(e)
+
+		var iaaInput = $(i.elem).prop('iaaInputNumberField');
+		
+		if (iaaInput != undefined){
+
+			var sing = -1;
+		
+			if (i.value > 0){
+
+				sing = 1;
+
+			}
+		
+			console.log(sing);
+		
+			iaaInput.addStep(sing);
+		
+		}
+		
+		return cancelEvent(e);
+
+	}
+
+	function snapMouseDoun(e){
+		
+		if (e.which == 1) salf._pressLeftMouseButton = true;
+		if (e.which == 2) {salf._pressMouseWheel = true; cancelEvent(e)};	
+		if (e.which == 3) {salf._pressRightMouseButton = true;  cancelEvent(e)};
+
+	}
+
+	function snapMouseMove(e){
+
+		var [sx,sy,sz] = [0,0,0]
+
+		if (salf._gx != undefined) {[sx, sy] = [e.clientX - salf._gx, e.clientY - salf._gy]}
+
+		[salf._gx, salf._gy] = [e.clientX, e.clientY]
+
+		if (e.which == 0){salf._pressRightMouseButton = false; salf._pressLeftMouseButton = false;salf._pressMouseWheel = false;}
+
+		salf.MapCommandButton.checkButton().svg_mousemove(sx*1.5, sy*1.5)
+		
+		if (salf._pressMouseWheel == true) leftToolBarTurn_svg_mousemove(sx, sy)
+		
+		if (salf._pressRightMouseButton == true) leftToolBarHand_svg_mousemove(sx, sy)
+
+	}
+
+	function snapMouseUp(e){
+
+		if (e.which == 1) salf._pressLeftMouseButton = false;
+		if (e.which == 2) salf._pressMouseWheel = false;
+		if (e.which == 3) salf._pressRightMouseButton = false;
+		
+		[salf._gx, salf._gy] = [undefined, undefined]
+		
+	}
+
+	function snapMouseLeave(e){
+
+		salf._pressLeftMouseButton = false;
+
+		salf._pressMouseWheel = false;
+		
+		salf._pressRightMouseButton = false;
+
+		[salf._gx, salf._gy] = [undefined, undefined]
+		
+	}
+
+	//.........
+	
 }
 
 // Группа //
@@ -184,6 +406,7 @@ function iaaGroup(){
 	this.parent = undefined;	// родительский элемент
 
 	this.name = '';
+	
 	
 	this.position = [0,0,0];	// позиция
 	
@@ -208,6 +431,28 @@ function iaaGroup(){
 	this.gridVisible = false; // отображать сетку
 	
 	this.gridStep = [50,50,50]; // шаг сетки
+	
+	this.repaint = function(){
+		
+		for(var i = 0; i < this.elements.length; i++){
+			
+			console.log(this.elements[i])
+			
+		}
+		
+	}
+
+	this.addElement = function(prop){
+		
+		var newElement = new iaaЕlement(prop.name, this);
+		
+		newElement.name 
+		
+		this.elements.push(newElement);
+		
+		myCanvas.currentDraft.repaint();
+		
+	}
 	
 }
 
@@ -264,7 +509,7 @@ function iaaDraft(name, w, h, d){
 	this.faceVisible = false;
 	
 	this.gridVisible = true;
-
+	
 }
 
 // Элемент //
@@ -277,10 +522,10 @@ function iaaЕlement(name, parentElement){
 
 	iaaGroup.apply(this, arguments);
 	
-	var _parent = parentElement; // родительский элемент
+	this.parent = parentElement; // родительский элемент
 	
-	var _name = name; // имя
-		
+	this.name = name; // имя
+
 }
 
 // Точка действия //
@@ -299,6 +544,18 @@ function iaaActionPoint(parentElement, parentFace){
 
 }
 
+// Времяночка //
 
+function elementProperty(name,x , y, z, w, h, d,rx, ry, rz){
+	
+	var salf = this;
 
-
+	this.name = name;
+	
+	this.position = [x , y, z]
+	this.sizes = [w, h, d]
+	this.turn = [rx, ry, rz]
+	
+	//console.log(arguments)
+	
+}
